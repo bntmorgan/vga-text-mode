f373df1056dfc458e502d598f7aba0088a3bfcbb
Benoît Morgan <bmorgan@laas.fr>
2016-02-22 17:06:42 +0059

    Corrected license file

--- sources/cores/vga/rtl/sim_vga.v
+++ sources/cores/vga/rtl/sim_vga.v
@@ -3,18 +3,18 @@ Copyright (C) 2015  Benoît Morgan
 
 This file is part of vga-text-mode.
 
-L2 is free software: you can redistribute it and/or modify
+vga-text-mode is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-L2 is distributed in the hope that it will be useful,
+vga-text-mode is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with L2.  If not, see <http://www.gnu.org/licenses/>.
+along with vga-text-mode.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 reg vga_clk;
