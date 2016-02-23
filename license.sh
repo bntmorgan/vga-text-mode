2c85587a6d84870168ab7f7e08910b2198d3409c
Benoît Morgan <bmorgan@laas.fr>
2016-02-23 15:46:21 +0059

    Better license

--- license.sh
+++ license.sh
@@ -13,3 +13,16 @@ for f in `find . -type f | grep -i "\.\(c\|h\|v\)$"`; do
     echo "License Header copied to $f"
   fi 
 done 
+
+for f in `find . -type f | grep -i "\(Makefile\|\.mk\)$"`; do
+  if (grep Copyright $f);then 
+    echo "No need to copy the License Header to $f"
+  else
+    cat LICENSE | awk '{if (NF) {printf "# ";print} else {print "#"}}' \
+        >> $f.new
+		echo "" >> $f.new
+		cat $f >> $f.new
+    mv $f.new $f
+    echo "License Header copied to $f"
+  fi 
+done 
--- sources/vga_demo/rules.mk
+++ sources/vga_demo/rules.mk
@@ -1,3 +1,20 @@
+# Copyright (C) 2015  Benoît Morgan
+#
+# This file is part of vga-text-mode.
+#
+# vga-text-mode is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# vga-text-mode is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with vga-text-mode.  If not, see <http://www.gnu.org/licenses/>.
+
 sp              	:= $(sp).x
 dirstack_$(sp)  	:= $(d)
 d               	:= $(dir)
